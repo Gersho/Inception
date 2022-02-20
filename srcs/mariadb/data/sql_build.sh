0a12608513eed78bca35f8d70d303f0641fe022d
Gersho <zennoune.karim@outlook.fr>
2022-02-20 20:14:39 +0100

    end of day

--- srcs/mariadb/data/sql_build.sh
+++ srcs/mariadb/data/sql_build.sh
@@ -1,31 +1,33 @@
-rm -rf inception.cnf
-touch inception.cnf 
+rm -rf .my.cnf
+touch .my.cnf
 echo "[mysqld]
 skip-networking=0
-bind-address = wordpress
-" >> inception.cnf
+bind-address = 0.0.0.0
+" >> .my.cnf
 
 echo "print"
-cp inception.cnf /etc/mysql/conf.d/
+cp .my.cnf /root
 echo "print 0"
 
 
 
 rm -rf basics.sql
 touch basics.sql
-echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'localhost' IDENTIFIED BY '$SQL_PASS';
+echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASS';
 FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS $SQL_NAME;
-GRANT ALL ON $SQL_NAME.* TO '$SQL_USER'@'wordpress' IDENTIFIED BY '$SQL_PASS';
+GRANT ALL ON $SQL_NAME.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASS';
 GRANT ALL ON *.* TO 'root'@'localhost';
-FLUSH PRIVILEGES;" >> basics.sql
-
-rm -rf basics2.sql
-touch basics2.sql
-
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOTPASS';
 FLUSH PRIVILEGES;
-" >> basics2.sql
+" >> basics.sql
+
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOTPASS';
+# rm -rf basics2.sql
+# touch basics2.sql
+
+# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOTPASS';
+# FLUSH PRIVILEGES;
+# " >> basics2.sql
 
 
 
@@ -34,7 +36,7 @@ service mysql start
 echo "print 1"
 mysql -u root < basics.sql
 echo "print 2"
-mysql -u root < basics2.sql
+# mysql -u root < basics2.sql
 echo "print 3"
-sleep infinity
-
+#sleep infinity
+mysqld_safe
